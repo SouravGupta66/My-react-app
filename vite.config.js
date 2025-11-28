780ece731899874256bc8b5665b1e88c321baca8
SouravGupta66 <souravdasgupta01626@gmail.com>
2025-11-28 11:40:46 +0600

    done

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
-  base: 'My-react-app',
+  base: '/My-react-app/',
   plugins: [react(), tailwindcss()],
 })
